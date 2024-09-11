b630cedeacce32d0f0d3753406ee58f1afd819cb
Ben Stevenson <ben.stevenson@auckland.ac.nz>
2024-09-12 11:55:39 +1200

    Fixing AIC function comments and tidying up some plotting code.

--- output.r
+++ output.r
@@ -32,16 +32,11 @@ aics <- calc.aics()
 ## - $aic is the AIC value for each species-model pair.
 ## - $diff is the difference between a model's AIC and the model with
 ##   the best AIC for that species.
-## - $converged indicates whether or not the optimisation algorithm
-##   converged.
-## - $fitted indicates whether or not we actually tried to fit the
-##   model (there's no point fitting some of the complicated models to
-##   species with low numbers of sightings).
 ## - $best has a TRUE entry in the best model (by AIC) for each
 ##   species.
-## - $best.converged can be ignored.
 aics
 
+## A vector of species names.
 s.names <- c("Bryde's", "Common dolphin", "Bottlenose dolphin", "Orca", "Whale", "Bryde's plus whale")
 ## Choose a species:
 ## 1 = "byrde",
@@ -79,106 +74,100 @@ for (i in 1:6){
 
 ## Plotting changes in occurrence probability at a specific location
 ## over time.
-do.occ <- FALSE
-if (do.occ){
     
-    ## Uncomment and run the code below to choose location(s)
-    ## by selecting a number from the following plot. You might need
-    ## to make the graphics window larger to see all the numbers.
-    #plot.surf(species = 1, model = 1, month = 1)
-    #text(mesh$loc[, 1], mesh$loc[, 2], labels = 1:nrow(mesh$loc), cex = 0.75)
-    ## To see the lat/long coordinates of each location, look in mesh$loc.
-    
-    ## The locations IDs from above go here.
-    ps <- c(310, 311, 292, 295)
-    n.plots <- length(ps)
-    cols <- brewer.pal(6, "Set1")
-    ## Mesh projection.
-    proj <- inla.mesh.projector(mesh)
-    ## Set to TRUE for same scale.
-    same.scale <- FALSE
-    ## Sorting out layout.
-    separate.maps <- TRUE
-    if (separate.maps){
-        mat.layout <- matrix(c(rep(1:(2*n.plots), times = rep(c(3, 1), n.plots))),
-                             ncol = 4, byrow = TRUE)
+## Uncomment and run the code below to choose location(s)
+## by selecting a number from the following plot. You might need
+## to make the graphics window larger to see all the numbers.
+#plot.surf(species = 1, model = 1, month = 1)
+#text(mesh$loc[, 1], mesh$loc[, 2], labels = 1:nrow(mesh$loc), cex = 0.75)
+## To see the lat/long coordinates of each location, look in mesh$loc.
+
+## The locations IDs from above go here.
+ps <- c(310, 311, 292, 295)
+n.plots <- length(ps)
+cols <- brewer.pal(6, "Set1")
+## Mesh projection.
+proj <- inla.mesh.projector(mesh)
+## Set to TRUE for same scale.
+same.scale <- FALSE
+## Sorting out layout.
+separate.maps <- TRUE
+if (separate.maps){
+    mat.layout <- matrix(c(rep(1:(2*n.plots), times = rep(c(3, 1), n.plots))),
+                         ncol = 4, byrow = TRUE)
+} else {
+    mat.layout <- cbind(matrix(rep(c(1, 3:(n.plots + 1)), each = 3),
+                               ncol = 3, byrow = TRUE),
+                        c(2, rep(0, n.plots - 1)))
+}
+layout(mat.layout, heights = rep(1, 4), widths = rep(1, 4))
+par(mar = c(0, 0, 0.25, 0.5), oma = c(3, 4, 0, 0.25), las = 1, xaxs = "i")
+all.ds <- array(0, dim = c(n.plots, n.months, 6))
+for (i in 1:n.plots){
+    p <- ps[i]
+    for (s in 1:6){
+        ## Note: probably need to set a species-specific model here.
+        m <- which(aics$best[s, ])
+        all.ds[i, , s] <- d.full[[s]][[m]][1, p, ]
+    }
+}
+for (i in 1:n.plots){
+    p <- ps[i]
+    p.loc <- mesh$loc[p, ]
+    plot.new()
+    if (same.scale){
+        ylim <- c(0, max(all.ds))
     } else {
-        mat.layout <- cbind(matrix(rep(c(1, 3:(n.plots + 1)), each = 3),
-                                   ncol = 3, byrow = TRUE),
-                            c(2, rep(0, n.plots - 1)))
+        ylim <- c(0, max(all.ds[i, , ]))
     }
-    layout(mat.layout, heights = rep(1, 4), widths = rep(1, 4))
-    par(mar = c(0, 0, 0.25, 0.5), oma = c(3, 4, 0, 0.25), las = 1, xaxs = "i")
-    all.ds <- array(0, dim = c(n.plots, n.months, 6))
-    for (i in 1:n.plots){
-        p <- ps[i]
-        for (s in 1:6){
-            ## Note: probably need to set a species-specific model here.
-            m <- which(aics$best[s, ])
-            all.ds[i, , s] <- d.full[[s]][[m]][1, p, ]
-        }
+    plot.window(xlim = c(1, n.months),
+                ylim = ylim)
+    abline(v = 6 + (0:30)*12, col = "lightgrey")
+    for (s in 1:6){
+        lines(1:n.months, all.ds[i, , s], col = cols[s])
     }
-    for (i in 1:n.plots){
-        p <- ps[i]
-        p.loc <- mesh$loc[p, ]
+    box()
+    if (i == n.plots){
+        axis(1, at = 6 + (0:19)*12, labels = FALSE)
+        axis(1, labels = as.character(2000:2019), at = 12*(0:19), tick = FALSE)
+        title(xlab = "Year")
+    }
+    axis(2)
+    if (i == 1){
+        legend("topright", c("Bryde's whale", "Common dolphin", "Bottlenose dolphin", "Orca", "Whale", "Bryde's +"),
+               col = cols, lty = rep(1, 6), bg = "white")
+    }
+    if (separate.maps){
         plot.new()
-        if (same.scale){
-            ylim <- c(0, max(all.ds))
-        } else {
-            ylim <- c(0, max(all.ds[i, , ]))
-        }
-        plot.window(xlim = c(1, n.months),
-                    ylim = ylim)
-        abline(v = 6 + (0:30)*12, col = "lightgrey")
-        for (s in 1:6){
-            lines(1:n.months, all.ds[i, , s], col = cols[s])
-        }
+        plot.window(xlim = range(proj$x), ylim = range(proj$y), asp = 1)
         box()
-        if (i == n.plots){
-            axis(1, at = 6 + (0:19)*12, labels = FALSE)
-            axis(1, labels = as.character(2000:2019), at = 12*(0:19), tick = FALSE)
-            title(xlab = "Year")
-        }
-        axis(2)
+        plot.coast()
+        pts.cols <- rep(cols[2], 4)
+        pts.cols[i] <- cols[1]
+        points(mesh$loc[ps, 1], mesh$loc[ps, 2], col = pts.cols, pch = 16)
+    } else {
         if (i == 1){
-            legend("topright", c("Bryde's whale", "Common dolphin", "Bottlenose dolphin", "Orca", "Whale", "Bryde's +"),
-                   col = cols, lty = rep(1, 6), bg = "white")
-        }
-        if (separate.maps){
             plot.new()
-            plot.window(xlim = range(proj$x), ylim = range(proj$y), asp = 1)
+            plot.window(xlim = bbox.small[, 1], ylim = bbox.small[, 2], asp = 1)
             box()
-            plot.coast()
-            pts.cols <- rep(cols[2], 4)
-            pts.cols[i] <- cols[1]
-            points(mesh$loc[ps, 1], mesh$loc[ps, 2], col = pts.cols, pch = 16)
-        } else {
-            if (i == 1){
-                plot.new()
-                plot.window(xlim = bbox.small[, 1], ylim = bbox.small[, 2], asp = 1)
-                box()
-                plot(NZ, col = "grey", add = TRUE)
-                text(mesh$loc[ps, 1], mesh$loc[ps, 2], labels = as.character(1:n.plots))
-            }
+            plot(NZ, col = "grey", add = TRUE)
+            text(mesh$loc[ps, 1], mesh$loc[ps, 2], labels = as.character(1:n.plots))
         }
     }
 }
 
-do.st.add <- TRUE
-if (do.st.add){
-    ## Plots of the spatial omega field and temporal psi field for the
-    ## st-add models.
-    s <- 1
-    m <- 11
-    plot.surf(species = s, model = m, surf = "omega")
+## Plots of the spatial omega field and temporal psi field for the
+## st-add models.
+s <- 1
+m <- 11
+plot.surf(species = s, model = m, surf = "omega")
 
-    ## Plotting temporal process.
-    psi.t.est <- rand.summary[[s]][[m]][rownames(rand.summary[[s]][[m]]) == "psi_t_all", 1]
-    plot(psi.t.est, type = "l")
-    abline(v = 12*(0:20), lty = "dotted")
-}
+## Plotting temporal process.
+psi.t.est <- rand.summary[[s]][[m]][rownames(rand.summary[[s]][[m]]) == "psi_t_all", 1]
+plot(psi.t.est, type = "l")
+abline(v = 12*(0:20), lty = "dotted")
 
-png("epsilon-space.png")
+## Plotting a spatial covariance function.
 cols <- brewer.pal(6, "Set1")
 for (s in 1:6){
     m <- which(aics$best[s, ])
@@ -193,9 +182,8 @@ for (s in 1:6){
     }
 }
 legend("topright", legend = s.names, col = cols, lty = rep(1, 6))
-dev.off()
 
-png("epsilon-time.png")
+## Plotting a temporal covariance function.
 for (s in 1:6){
     m <- which(aics$best[s, ])
     link.phi <- fit[[s]][[m]]$par["link_phi_epsilon"]
@@ -211,4 +199,3 @@ for (s in 1:6){
 }
 abline(h = 0, lty = "dotted")
 legend("bottomright", legend = s.names, col = cols, pch = rep(1, 6))
-dev.off()
