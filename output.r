545048bc75e1bd293d9ac6c411f3a801d6b7882b
Ben Stevenson <ben.stevenson@auckland.ac.nz>
2025-02-18 17:26:01 +1300

    Plot adjustments.

--- output.r
+++ output.r
@@ -37,7 +37,7 @@ aics <- calc.aics()
 aics
 
 ## A vector of species names.
-s.names <- c("Bryde's", "Common dolphin", "Bottlenose dolphin", "Orca", "Whale", "Bryde's plus whale")
+s.names <- c("Bryde's whale", "Common dolphin", "Bottlenose dolphin", "Killer Whale", "Whale", "Bryde's whale +")
 ## Choose a species:
 ## 1 = "byrde",
 ## 2 = "cdolp",
@@ -47,30 +47,30 @@ s.names <- c("Bryde's", "Common dolphin", "Bottlenose dolphin", "Orca", "Whale",
 ## 6 = "brydeplus"
 ## Choose a model via names(fit[[1]])
 ## Plotting estimated relative density for a particular month/species/model.
-plot.surf(species = 6, model = 8, month = 100, show.obs = TRUE)
+#plot.surf(species = 6, model = 8, month = 100, show.obs = TRUE)
 ## Plotting estimated spatially varying effect of temperature for a
 ## particular species/model. Only makes sense for models that include
 ## such an effect.
-plot.surf(species = 1, model = 16, surf = "int")
+#plot.surf(species = 1, model = 16, surf = "int")
 ## Plotting estimated spatial effect. This only makes sense for model
 ## that have a single constant wiggly spatial field over time.
-plot.surf(species = 3, model = 13, surf = "omega")
+#plot.surf(species = 3, model = 13, surf = "omega")
 
 ## Make a distribution gif for a species/model combination. Here I'm
 ## making a gif for each species, using the best model for that
 ## species. Takes a while to run.
-for (i in 1:6) save.gif(species = i, model = which(aics$best[i, ]), show.obs = TRUE)
+#for (i in 1:6) save.gif(species = i, model = which(aics$best[i, ]), show.obs = TRUE)
 ## Plot survey effort.
-plot.effort()
+#plot.effort()
 
 ## For example, here's a plot of the spatially varying effect of
 ## temperature for all species.
-par(mfrow = c(3, 2))
+#par(mfrow = c(3, 2))
 ## Just using the model 16 for these, which isn't actually the best
 ## model for every species.
-for (i in 1:6){
-    plot.surf(species = i, model = 16, surf = "int")
-}
+#for (i in 1:6){
+#    plot.surf(species = i, model = 16, surf = "int")
+#}
 
 ## Plotting changes in occurrence probability at a specific location
 ## over time.
@@ -83,9 +83,10 @@ for (i in 1:6){
 ## To see the lat/long coordinates of each location, look in mesh$loc.
 
 ## The locations IDs from above go here.
+pdf(file = "ts-plot.pdf", width = 7)
 ps <- c(310, 311, 292, 295)
 n.plots <- length(ps)
-cols <- brewer.pal(6, "Set1")
+cols <- brewer.pal(7, "Set1")[-6][order(order(s.names))]
 ## Mesh projection.
 proj <- inla.mesh.projector(mesh)
 ## Set to TRUE for same scale.
@@ -101,11 +102,11 @@ if (separate.maps){
                         c(2, rep(0, n.plots - 1)))
 }
 layout(mat.layout, heights = rep(1, 4), widths = rep(1, 4))
-par(mar = c(0, 0, 0.25, 0.5), oma = c(3, 4, 0, 0.25), las = 1, xaxs = "i")
+par(mar = c(0, 0, 0.25, 0.5), oma = c(6, 6, 0, 0.25), las = 1, xaxs = "i")
 all.ds <- array(0, dim = c(n.plots, n.months, 6))
 for (i in 1:n.plots){
     p <- ps[i]
-    for (s in 1:6){
+    for (s in order(s.names)){
         ## Note: probably need to set a species-specific model here.
         m <- which(aics$best[s, ])
         all.ds[i, , s] <- d.full[[s]][[m]][1, p, ]
@@ -123,28 +124,29 @@ for (i in 1:n.plots){
     plot.window(xlim = c(1, n.months),
                 ylim = ylim)
     abline(v = 6 + (0:30)*12, col = "lightgrey")
-    for (s in 1:6){
+    for (s in order(s.names)){
         lines(1:n.months, all.ds[i, , s], col = cols[s])
     }
     box()
     if (i == n.plots){
         axis(1, at = 6 + (0:19)*12, labels = FALSE)
-        axis(1, labels = as.character(2000:2019), at = 12*(0:19), tick = FALSE)
-        title(xlab = "Year")
+        axis(1, labels = as.character(2000:2019), at = 12*(0:19), tick = FALSE, las = 3)
+        mtext("Year", side = 1, line = 4, srt = 90, las = 0)
+        mtext("p(s, t)", side = 2, line = 4, las = 0, outer = TRUE)
     }
     axis(2)
     if (i == 1){
-        legend("topright", c("Bryde's whale", "Common dolphin", "Bottlenose dolphin", "Orca", "Whale", "Bryde's +"),
-               col = cols, lty = rep(1, 6), bg = "white")
+        legend("topright", sort(s.names),
+               col = cols[order(s.names)], lty = rep(1, 6), bg = "white")
     }
     if (separate.maps){
         plot.new()
         plot.window(xlim = range(proj$x), ylim = range(proj$y), asp = 1)
         box()
         plot.coast()
-        pts.cols <- rep(cols[2], 4)
-        pts.cols[i] <- cols[1]
-        points(mesh$loc[ps, 1], mesh$loc[ps, 2], col = pts.cols, pch = 16)
+        pts.cols <- rep(cols[1], 4)
+        pts.cols[i] <- cols[3]
+        points(mesh$loc[ps, 1], mesh$loc[ps, 2], col = pts.cols, pch = 16, cex = 2)
     } else {
         if (i == 1){
             plot.new()
@@ -155,6 +157,7 @@ for (i in 1:n.plots){
         }
     }
 }
+dev.off()
 
 ## Plots of the spatial omega field and temporal psi field for the
 ## st-add models.
@@ -168,34 +171,37 @@ plot(psi.t.est, type = "l")
 abline(v = 12*(0:20), lty = "dotted")
 
 ## Plotting a spatial covariance function.
-cols <- brewer.pal(6, "Set1")
-for (s in 1:6){
+pdf("space-cov.pdf")
+for (s in order(s.names)){
     m <- which(aics$best[s, ])
     kappa <- exp(fit[[s]][[m]]$par["log_kappa_epsilon"])
     dd <- seq(0, 1, length.out = 1000)
     yy <- matern.cov(dd, 1, kappa)
     lat.km <- 110.574
-    if (s == 1){
+    if (s == order(s.names)[1]){
         plot(lat.km*dd, yy, type = "l", xlab = "Distance (km)", ylab = "Correlation", ylim = c(0, 1), col = cols[s])
     } else {
         lines(lat.km*dd, yy, col = cols[s])
     }
 }
-legend("topright", legend = s.names, col = cols, lty = rep(1, 6))
+legend("topright", legend = sort(s.names), col = cols[order(s.names)], lty = rep(1, 6))
+dev.off()
 
 ## Plotting a temporal covariance function.
-for (s in 1:6){
+pdf("temp-cov.pdf")
+for (s in order(s.names)){
     m <- which(aics$best[s, ])
     link.phi <- fit[[s]][[m]]$par["link_phi_epsilon"]
     phi <- 2*exp(link.phi)/(1 + exp(link.phi)) - 1
     tt <- 0:12
     yy <- phi^tt
-    if (s == 1){
-        plot(tt, yy, ylim = c(-0.5, 1), col = cols[s], xlab = "Time (months)", ylab = "Correlation")
+    if (s == order(s.names)[1]){
+        plot(tt, yy, ylim = c(-0.5, 1), col = cols[s], xlab = "Time (months)", ylab = "Correlation", type = "b")
     } else {
-        points(tt, yy, col = cols[s])
+        points(tt, yy, col = cols[s], type = "b")
     }
     print(phi)
 }
 abline(h = 0, lty = "dotted")
-legend("bottomright", legend = s.names, col = cols, pch = rep(1, 6))
+legend("bottomright", legend = sort(s.names), col = cols[order(s.names)], pch = rep(1, 6))
+dev.off()
